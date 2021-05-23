586d9fec27fe1328117296bf6a7c7c94458134cd
RonaldColyar <phantexcreations@gmail.com>
2021-05-23 11:23:30 -0400

    added init

--- type_capabilities.py
+++ type_capabilities.py
@@ -1,2 +1,8 @@
 class Capabilities:
-    pass
+    def __init__(self,has_pir = False,
+        has_video_streaming = False,has_audio_streaming = False,
+        has_ground_movement=False):
+        self.has_pir = has_pir
+        self.has_video_streaming = has_video_streaming
+        self.has_audio_streaming = has_audio_streaming
+        self.has_ground_movement = has_ground_movement
