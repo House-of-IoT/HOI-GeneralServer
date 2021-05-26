3b3c15170e378e8faccfe6af86c2cfb51b095d91
RonaldColyar <phantexcreations@gmail.com>
2021-05-26 08:44:16 -0400

    advancement

--- type_capabilities.py
+++ type_capabilities.py
@@ -2,7 +2,9 @@ class Capabilities:
     def __init__(self,has_pir = False,
         has_video_streaming = False,has_audio_streaming = False,
         has_ground_movement=False):
-        self.has_pir = has_pir
-        self.has_video_streaming = has_video_streaming
-        self.has_audio_streaming = has_audio_streaming
-        self.has_ground_movement = has_ground_movement
+        self.functionality = {
+            "video_streaming" : has_video_streaming,
+            "audio_streaming" : has_audio_streaming,
+            "ground_movement" : has_ground_movement,
+            "pir" : has_pir
+        } 
