1f7efe364eaba6e430fbb95764825cfec174ef2d
RonaldColyar <phantexcreations@gmail.com>
2021-07-01 00:33:58 -0400

    response object

--- BasicResponse.py
+++ BasicResponse.py
@@ -1,3 +1,5 @@
+import json
+
 class BasicResponse:
     def __init__(self,server_name):
         self.server_name = server_name
@@ -6,4 +8,13 @@ class BasicResponse:
         self.status = None
         self.bot_name = None
 
+    def string_version(self):
+        data_dict = {
+            "server_name" : self.server_name,
+            "action" : self.action,
+            "response" : self.response,
+            "status" : self.status,
+            "bot_name" : self.bot_name
+        }
 
+        return json.loads(data_dict)
