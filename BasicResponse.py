e993ea32382399cfdf253f743fc6c4c64e827fcb
RonaldColyar <phantexcreations@gmail.com>
2021-07-01 00:21:19 -0400

    response object

---
+++ BasicResponse.py
@@ -0,0 +1,9 @@
+class BasicResponse:
+    def __init__(self,server_name):
+        self.server_name = server_name
+        self.action = None
+        self.response = None
+        self.status = None
+        self.bot_name = None
+
+
