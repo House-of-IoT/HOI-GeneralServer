90c6832a1400124c068df4d60d10bad3e5a1144c
RonaldColyar <phantexcreations@gmail.com>
2021-08-03 22:12:59 -0500

    deprecated notify timeout

--- client_handler.py
+++ client_handler.py
@@ -34,64 +34,58 @@ class ClientHandler:
                 
     #sending the server's live table state
     async def send_table_state(self,table_or_set,target,keys_or_values_or_both):
-        state_response = BasicResponse(self.parent.outside_names[self.name])
-        state_response.target = target
-        state_response.action = "viewing"
         if(await self.client_has_credentials("viewing")):
             try:
-                state_response.status = "success"
+                target_value = None
                 if keys_or_values_or_both == "keys":
-                    state_response.target_value = table_or_set.keys()
+                    target_value = table_or_set.keys()
                 elif keys_or_values_or_both == "values":
-                    state_response.target_value = table_or_set.values()
+                    target_value = table_or_set.values()
                 elif keys_or_values_or_both == "values-set":
-                    state_response.target_value = list(table_or_set)
+                    target_value = list(table_or_set)
                 else:
-                     state_response.target_value = json.dumps(table_or_set)
-                await asyncio.wait_for(self.websocket.send(state_response.string_version()),10)
+                     target_value = json.dumps(table_or_set)
+                await self.send_basic_response("success",action = "viewing", target= target, target_value= target_value)
             except Exception as e:
                 print(e)
-                await self.notify_timeout(state_response)
+                await self.send_basic_response("timeout",action= "viewing",target=target)
         else:
-            state_response.status = "failed-auth"
-            await asyncio.wait_for(self.websocket.send(state_response.string_version()),40)
+            await self.send_basic_response("failed-auth",action= "viewing",target=target)
 
     async def send_server_config(self):
         try:
-            state_response = BasicResponse(self.parent.outside_names[self.name])
-            state_response.action = "viewing"
-            state_response.target = "server_config"
-            state_response.status = "success"
-            state_response.target_value = self.parent.config.string_version()
-            await asyncio.wait_for(self.websocket.send(state_response.string_version()),40)
+            await self.send_basic_response(
+                "success",
+                action= "viewing",
+                target="server_config" ,
+                target_value=self.parent.config.string_version())
         except Exception as e:
             print(e)
-            await self.notify_timeout(state_response)
+            self.parent.console_logger.log_generic_row(f"A request that {self.name} made has timed out!","red")
+            await self.send_basic_response("timeout", action= "viewing",target="server_config")
             
     #editing the server's live config settings
     async def handle_config_request(self,request):
-        response = BasicResponse(self.parent.outside_names[self.name])
-        response.action = "editing"
+        status = None
         try:
             new_boolean = bool(await asyncio.wait_for(self.websocket.recv(),40))      
             successfully_authed_with_super_pass = self.send_need_admin_auth_and_check_response(self.parent.super_admin_password,"editing")
 
             if successfully_authed_with_super_pass:
                 self.modify_matching_config_boolean(request,new_boolean)
-                response.status = "success"
+                status = "success"
             else:
-                response.status = "failure"
-            await asyncio.wait_for(self.websocket.send(response.string_version(),40))
+                status = "failure"
+            await self.send_basic_response(status,action="editing",target=request)
 
         except Exception as e:
             traceback.print_exc()
             if e is AddressBannedException:
                 raise e
-            else:
-                response.status = "timeout"
+            else:   
                 traceback.print_exc()
-                await self.notify_timeout(response)
-
+                self.parent.console_logger.log_generic_row(f"A request that {self.name} made has timed out!","red")
+                await self.send_basic_response("timeout",action= "editing", target=request)
 #PRIVATE
     async def check_for_stop(self,bot_name):
         try:
@@ -113,23 +107,17 @@ class ClientHandler:
             await self.stream(bot_name,action)
 
     async def activate_deactivate_or_disconnect_bot(self,bot_name,action):
-        server_name = self.parent.outside_names[self.name]
-        basic_response = BasicResponse(server_name)
-        basic_response.action = action
-        basic_response.bot_name = bot_name
         if(await self.client_has_credentials(action)):
             #send bot the basic request
             bot_connection  = self.parent.devices[bot_name]
             await asyncio.wait_for(bot_connection.send(action),10)
             status = await asyncio.wait_for(bot_connection.recv(),10);
             self.parent.console_logger.log_generic_row(f"bot({bot_name}) responded with {status} to {self.name}'s action request:{action}\n","green")
-            basic_response.status = status
             #send client the result
-            await asyncio.wait_for(self.websocket.send(basic_response.string_version()),10)
+            await self.send_basic_response(status,action= action,bot_name=bot_name)
             self.handle_activate_deactivate_or_disconnect_cleanup(bot_name,action,status)
         else:
-            basic_response.status = "failure"
-            await asyncio.wait_for(self.websocket.send(basic_response.string_version()),10)
+            await self.send_basic_response("failure",action= action,bot_name=bot_name)
         
     def handle_activate_deactivate_or_disconnect_cleanup(self,bot_name,action,status):
         if status == "success":
@@ -240,17 +228,19 @@ class ClientHandler:
             self.parent.config.disconnecting_requires_admin = new_boolean
 
     async def send_need_admin_auth_and_check_response(self,password,action):
-        basic_response = BasicResponse(self.parent.outside_names[self.name])
-        basic_response.action = action
-        basic_response.status = "needs-admin-auth"
-        await asyncio.wait_for(self.websocket.send(basic_response.string_version()),40)
+        await self.send_basic_response("needs-admin-auth",action = action)
         if await self.parent.is_authed(self.websocket,password):
             return True
         else:
             return False
 
-    #on failure , the outer block will close the connection for notifies
-    async def notify_timeout(self,response):
-        response.status = "timeout"
-        self.parent.console_logger.log_generic_row(f"A request that {self.name} made has timed out!","red")
-        await asyncio.wait_for(self.websocket.send(response.string_version()),10)
+    async def send_basic_response(
+        self,status,action = None,
+        target = None,target_value = None ,bot_name = None):
+        basic_response = BasicResponse(self.parent.outside_names[self.name])
+        basic_response.action = action
+        basic_response.status = status
+        basic_response.bot_name = bot_name
+        basic_response.target = target
+        basic_response.target_value = target_value
+        await asyncio.wait_for(self.websocket.send(basic_response.string_version()),40)
