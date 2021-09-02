d41f9116347bb0dce8f42c7f4228951cec1e3a14
RonaldColyar <phantexcreations@gmail.com>
2021-09-02 17:40:30 -0500

    wip-action cross

---
+++ action_utils.py
@@ -0,0 +1,21 @@
+import asyncio
+
+class ActionUtils:    
+    
+    async def send_need_admin_auth_and_check_response(self,password,action):
+        await self.send_basic_response("needs-admin-auth",action = action)
+        if await self.parent.is_authed(self.websocket,password):
+            return True
+        else:
+            return False 
+
+    async def send_basic_response(
+        self,status,action = None,
+        target = None,target_value = None ,bot_name = None):
+        basic_response = BasicResponse(self.parent.outside_names[self.name])
+        basic_response.action = action
+        basic_response.status = status
+        basic_response.bot_name = bot_name
+        basic_response.target = target
+        basic_response.target_value = target_value
+        await asyncio.wait_for(self.websocket.send(basic_response.string_version()),40)
