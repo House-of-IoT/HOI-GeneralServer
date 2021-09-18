d8ab658e3ab64df7d17b248c6547a08ad6bc710f
RonaldColyar <phantexcreations@gmail.com>
2021-09-18 07:15:02 -0500

    split execute logic

--- HybridActionsAndAutoScheduling/auto_scheduler.py
+++ HybridActionsAndAutoScheduling/auto_scheduler.py
@@ -1,5 +1,6 @@
-import datetime
 import asyncio
+import datetime
+
 
 class AutoScheduler:
     def __init__(self,interval,parent):
@@ -14,29 +15,35 @@ class AutoScheduler:
     def cancel_task(self,task):
         uid = str(task.time) + task.bot_name + task.action
         del self.tasks[uid]
+        
+    def task_should_run(self,task):
+        #if the task's time has passed or the time is now
+        if task.time <= datetime.datetime().utcnow():
+            # if bot is connected and available
+            if task.name in self.parent.devices and self.parent.available_status[task.name] == True:
+                return True
+        return False
     
     async def execute_tasks(self):
         tasks = list(self.tasks.keys())
         for task in tasks:
             #if the scheduled time has passed or it is the scheduled time
-            if task.time >= datetime.datetime():
-                # if bot is connected and available
-                if task.name in self.parent.devices and self.parent.available_status[task.name] == True:
-                    self.parent.available_status[task.name] = False
-                    try:
-                        #send the request and gather response
-                        bot_websocket = self.parent.devices[task.name]
-                        await asyncio.wait_for(bot_websocket.send(task.action),5)
-                        response = await asyncio.wait_for(bot_websocket.recv(),8)
-                        self.parent.most_recent_scheduled_tasks[task.name] = response
-                    except:
-                        self.parent.most_recent_scheduled_tasks[task.name] = "failure"
+            if self.task_should_run(task):
+                self.parent.available_status[task.name] = False
+                try:
+                    #send the request and gather response
+                    bot_websocket = self.parent.devices[task.name]
+                    await asyncio.wait_for(bot_websocket.send(task.action),5)
+                    response = await asyncio.wait_for(bot_websocket.recv(),8)
+                    self.parent.most_recent_scheduled_tasks[task.name] = response
+                except:
+                    self.parent.most_recent_scheduled_tasks[task.name] = "failure"
+                    
+                #release control of the bot
+                self.parent.available_status[task.name] = True
 
-                    #release control of the bot
-                    self.parent.available_status[task.name] = True
-
-                    #only try to execute one task at a time 
-                    break
+                #only try to execute one task at a time 
+                break
                 
 class Task:
     def __init__(self,time,name,action):
--- HybridActionsAndAutoScheduling/tests.py
+++ HybridActionsAndAutoScheduling/tests.py
@@ -1,6 +1,7 @@
 # This module aims at testing the underlying logic of the auto scheduler.
 
 import unittest
-
+import datetime
+import auto_scheduler
 class AsyncTests(unittest.IsolatedAsyncioTestCase):
     pass
