baf37e725339eb49c2487511abad2d646aaa4700
RonaldColyar <PhantexCreations@gmail.com>
2021-11-05 13:04:02 -0500

    data

--- DataCapture/queries.py
+++ DataCapture/queries.py
@@ -8,7 +8,39 @@ Note: SQL Injection isn't a concern due to the nature of HOI.
 
 class Queries:
     create_notification_table = '''
-    
-
-
+        CREATE TABLE IF NOT EXISTS notifications(
+            Id INTEGER PRIMARY KEY,
+            Name VARCHAR(20) NOT NULL,
+            Description text NOT NULL,
+            Date TIMESTAMP
+        );
     '''
+    #For autoscheduling only
+    create_task_execution_history_table = '''
+        CREATE TABLE IF NOT EXISTS tasks(
+            Id INTEGER PRIMARY KEY,
+            BotName VARCHAR(50) NOT NULL,
+            Action VARCHAR(50) NOT NULL,
+            Date TIMESTAMP
+        );
+    '''
+    
+    create_contacts_history = """
+        CREATE TABLE IF NOT EXISTS contacts(
+            Id INTEGER PRIMARY KEY,
+            Name VARCHAR(20) NOT NULL,
+            Number VARCHAR 20 NOT NULL
+        );
+    """
+
+    #for users only
+    create_action_execution_history = """
+        CREATE TABLE IF NOT EXISTS actions(
+            Id INTEGER PRIMARY KEY,
+            Executor VARCHAR(20) NOT NULL,
+            Action VARCHAR(50) NOT NULL,
+            BotName VARCHAR(50) NOT NULL,
+            Date TIMESTAMP
+        );
+    
+    """
