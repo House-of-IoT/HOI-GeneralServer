07660c69021d9ca441d8c75c0b2f0a0dd8d9318d
RonaldColyar <PhantexCreations@gmail.com>
2021-11-05 08:26:56 -0500

    sql init

---
+++ DataCapture/queries.py
@@ -0,0 +1,14 @@
+
+"""
+Note: SQL Injection isn't a concern due to the nature of HOI.
+1.Once someone has access to the general server, they can access all data
+    so there is no restricted data to be accessed.
+    The only people connected to the server should be trusted members of the server.
+"""
+
+class Queries:
+    create_notification_table = '''
+    
+
+
+    '''
---
+++ DataCapture/sql_lite_handler.py
@@ -0,0 +1,9 @@
+import sqlite3
+from queries import Queries
+
+class SQLiteHandler:
+    def __init__(self):
+        pass
+
+
+        
