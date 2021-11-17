dd7f9830c36476efff1999bdff6fd9ecaac7a93d
RonaldColyar <phantexcreations@gmail.com>
2021-11-16 21:59:45 -0600

    init

---
+++ DataCapture/capture_object_creator.py
@@ -0,0 +1,12 @@
+
+"""
+TBD: Futher Capture expansion
+"""
+class CaptureObjectCreator:
+
+    @staticmethod
+    def create_basic_object(type_data,data):
+        return {
+            "type":type_data,
+            "data":data
+        }
