adb827a5ff34f16a0089c45cdeecb22cb170d45f
RonaldColyar <phantexcreations@gmail.com>
2021-11-21 04:10:02 -0600

    special action init

---
+++ SpecialActions/special_action_handler.py
@@ -0,0 +1,10 @@
+
+"""
+The class that handles all special action protocols that aren't "basic".
+"basic" actions are ones that involves one request and one response, 
+"special" actions require more communication.
+"""
+
+class SpecialActionHandler:
+    def __init__(self):
+        pass
