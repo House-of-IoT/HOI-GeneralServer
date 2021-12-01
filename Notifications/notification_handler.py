e81ce6e6898eea739e1ad6789c5f432e80e31f59
RonaldColyar <phantexcreations@gmail.com>
2021-12-01 03:32:03 -0600

    notification implementation wip

--- Notifications/notification_handler.py
+++ Notifications/notification_handler.py
@@ -1,13 +1,33 @@
 from datetime import datetime
+import asyncio
 
 class NotificationHandler:
-    def __init__(self):
+    def __init__(self,parent):
         self.current_notifications = {}
+        self.parent = parent
     
-    def add_generic_notification_to_all(self,notification):
-        pass
-    
-    def create_notification(self,message):
-        notification = {"message":message, "time":datetime.utcnow()}
+    def create_notification(self,message,fatal):
+        notification = {"message":message, "time":datetime.utcnow(), "fatal":fatal}
         for key in self.current_notifications.keys():
-            self.current_notifications[key].append(notification)    
+            self.current_notifications[key].append(notification)
+    
+    def serve_notifications(self,name):
+        if name in self.current_notifications:
+            self.convert_notification_times_to_str(self.current_notifications[name])
+            notifications = self.current_notifications[name]
+            #reset notifications
+            self.current_notifications[name] = []
+            return notifications
+        else:
+            return []
+    
+    def convert_notification_times_to_str(self,notifications):
+        for notification in notifications:
+            notification["time"] = str(notification["time"])
+
+    async def remove_old_notifications(self):
+        while True:
+            await asyncio.sleep(120)
+            for key in self.current_notifications.keys():
+                if key not in self.parent.devices or "ExternalMonitor" in key:
+                    del self.current_notifications[key]
