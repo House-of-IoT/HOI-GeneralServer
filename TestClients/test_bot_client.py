87fc3c67c16e7fc69500e51d963d3616eb9d5b2e
RonaldColyar <phantexcreations@gmail.com>
2022-05-14 19:34:48 -0600

    wip

--- TestClients/test_bot_client.py
+++ TestClients/test_bot_client.py
@@ -34,8 +34,8 @@ protocol
 class AsyncTests(unittest.IsolatedAsyncioTestCase):
     
     async def connect(self):
-        websocket = await websockets.connect('ws://localhost:50223', ping_interval= None, max_size = 20000000)
-        await websocket.send("")
+        websocket = await websockets.connect('ws://localhost:50123', ping_interval= None, max_size = 20000000)
+        await websocket.send("t")
         await websocket.send(self.name_and_type())
         await websocket.send("test_name")
         connection_response = await websocket.recv()
